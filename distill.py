33e17c70785ce821a1f716797660b7789e7965b7
crispianm <45373428+crispianm@users.noreply.github.com>
2023-03-03 13:44:35 +0000

    Add distill.py

--- distill.py
+++ distill.py
@@ -1,5 +1,5 @@
 from data.datasets import *
-from trainers.quintuplet import Trainer
+from trainers.distiller import Distiller
 from torch.utils.data import DataLoader
 import argparse
 import torch
@@ -11,25 +11,48 @@ from os.path import join
 parser = argparse.ArgumentParser(description="STMFNet")
 
 # parameters
-# model
-parser.add_argument("--net", type=str, default="STMFNet")
+# Distillation options
+# student
+parser.add_argument("--student", type=str, default="student_STMFNet")
+parser.add_argument("--temp", type=int, default=5)
+
+# teacher
+parser.add_argument("--teacher", type=str, default="STMFNet")
+parser.add_argument("--teacher_dir", type=str, default="./models/stmfnet.pth")
+
+# distillation loss function
+parser.add_argument(
+    "--distill_loss_fn",
+    type=str,
+    default="1*GAN",
+    help="Loss function of difference between soft student predictions and soft teacher predictions",
+)
+parser.add_argument(
+    "--student_loss_fn",
+    type=str,
+    default="1*Lap",
+    help="distillation loss function configuration",
+)
+parser.add_argument(
+    "--alpha",
+    type=float,
+    default=0.1,
+    help="weight to student loss function (loss) and 1-alpha to distill_loss_fn",
+)
 
 # Hardware Setting
 parser.add_argument("--gpu_id", type=int, default=0)
 
-# Directory Setting
+# Directory Setting (load applies to student only)
 parser.add_argument(
-    "--data_dir", type=str, default="D:/stmfnet_data", help="root dir for all datasets"
+    "--data_dir", default="D:/stmfnet_data", type=str, help="root dir for all datasets"
 )
-parser.add_argument("--out_dir", type=str, default="./train_results")
+parser.add_argument("--out_dir", type=str, default="./distill_results")
 parser.add_argument("--load", type=str, default=None)
 
 # Learning Options
 parser.add_argument("--epochs", type=int, default=70, help="Max Epochs")
 parser.add_argument("--batch_size", type=int, default=2, help="Batch size")
-parser.add_argument(
-    "--loss", type=str, default="1*Lap", help="loss function configuration"
-)
 parser.add_argument("--patch_size", type=int, default=256, help="crop size")
 
 # Optimization specifications
@@ -55,8 +78,8 @@ parser.add_argument(
 parser.add_argument(
     "--optimizer",
     default="ADAMax",
-    choices=("SGD", "ADAM", "RMSprop", "ADAMax", "OBProxSG"),
-    help="optimizer to use (SGD | ADAM | RMSprop | ADAMax | OBProxSG)",
+    choices=("SGD", "ADAM", "RMSprop", "ADAMax"),
+    help="optimizer to use (SGD | ADAM | RMSprop | ADAMax)",
 )
 parser.add_argument("--weight_decay", type=float, default=0, help="weight decay")
 
@@ -94,7 +117,7 @@ def main():
         augment_t=False,
     )
 
-    datasets_train = [vimeo90k_train, bvidvc_train]
+    datasets_train = [bvidvc_train, vimeo90k_train]
     train_sampler = Sampler(datasets_train, iter=True)
 
     # data loaders
@@ -105,19 +128,29 @@ def main():
         dataset=vimeo90k_valid, batch_size=args.batch_size, num_workers=0
     )
 
-    # model and loss function
-    model = getattr(models, args.net)(args).cuda()
-    print("****** Model created ******")
+    # load teacher model
+    teacher = getattr(models, args.teacher)(args).cuda()
+    checkpoint = torch.load(args.teacher_dir)
+    teacher.load_state_dict(checkpoint["state_dict"])
 
-    loss = losses.Loss(args)
+    # student and loss function
+    student = getattr(models, args.student)(args).cuda()
+    print("******student net: ", args.student, " created******")
+    print("Using alpha =", args.alpha)
+    # print('Using distillation loss function: ', args.distill_loss_fn)
+    # print('Using student loss function: ', args.student_loss_fn)
+
+    loss = losses.DistillationLoss(args)
 
     start_epoch = 0
     if args.load is not None:
         checkpoint = torch.load(args.load)
-        model.load_state_dict(checkpoint["state_dict"])
+        student.load_state_dict(checkpoint["state_dict"])
         start_epoch = checkpoint["epoch"]
 
-    my_trainer = Trainer(args, train_loader, valid_loader, model, loss, start_epoch)
+    my_distiller = Distiller(
+        args, train_loader, valid_loader, student, teacher, loss, start_epoch
+    )
 
     now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     with open(join(args.out_dir, "config.txt"), "a") as f:
@@ -126,10 +159,10 @@ def main():
             f.write("{}: {}\n".format(arg, getattr(args, arg)))
         f.write("\n")
 
-    while not my_trainer.terminate():
-        my_trainer.train()
-        my_trainer.validate()
-        my_trainer.save_checkpoint()
+    while not my_distiller.terminate():
+        my_distiller.train()
+        my_distiller.save_checkpoint()
+        my_distiller.validate()
 
 
 if __name__ == "__main__":
